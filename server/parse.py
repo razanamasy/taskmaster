6c5500c61a37ad6bc066b3031f4cb88a57502e40
alice <alice@Kfs.com>
2023-04-28 10:54:53 -0700

    finished parsing have to correct umask and signal casting issue

---
+++ server/parse.py
@@ -0,0 +1,51 @@
+import yaml
+import sys
+from process_struct import *
+
+def parse_file(configs):
+    process_dict = {}
+    if "programs" in configs == False:
+        print("No programs section")
+        exit(1)
+    else:
+        for glob, programs in configs.items():
+            if isinstance(programs, dict) and programs != "programs":
+                for proc_name, conf in programs.items():
+                    if isinstance(conf, dict):
+                        process_dict[proc_name] = process(proc_name)
+                        for key, value in conf.items():
+                            if isinstance(value, dict):
+                                print("Wrong config file3")
+                                exit(1)
+                            else:
+                                setattr(process_dict[proc_name], key, value)
+                    else:
+                        print("Wrong config file2")
+                        exit(1)
+            else:
+                print("Wrong config file1")
+                exit(1)
+    print(process_dict["ls"]);
+    print(process_dict["env"]);
+        
+
+def open_file(conf_file):
+    try:
+        with open(conf_file, 'r') as f:
+            configs = yaml.safe_load(f)
+            parse_file(configs)
+    except FileNotFoundError:
+        print("File not found")
+        exit(1)
+    except IOError:
+        print("Could not read file")
+        exit(1)
+    except PermissionError:
+        print("You don't have the permissions to read the file")
+        exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Wrong number of arguments")
+        exit(1)
+    open_file(sys.argv[1]);
---
+++ server/process_struct.py
@@ -0,0 +1,29 @@
+class process:
+    def __init__(self, name):
+        self.name = name
+        self.cmd = ""
+        self.numprocs = 1
+        self.umask = "022"
+        self.workingdir = ""
+        self.autostart = True
+        self.autorestart = "unexpected"
+        self.exitcode = [0]
+        self.startretries = 3
+        self.starttime = 1
+        self.stopsignal = "TERM"
+        self.stoptime = 10
+        self.stdout = "/etc/taskmaster/stdout/" + name
+        self.stderr = "/etc/taskmaster/stderr/" + name
+        self.env = []
+        self.failure = False
+        self.fatal = False
+        self.logs = []
+
+    def __str__(self):
+        return f"My class: \n name = {self.name}\n cmd = {self.cmd}\n numprocs = {self.numprocs}\n \
+umask = {self.umask}\n workingdir = {self.workingdir}\n autostart = {self.autostart}\n \
+autorestart = {self.autorestart}\n exitcode = {self.exitcode}\n startretries = {self.startretries}\n \
+starttime = {self.starttime}\n stopsignal = {self.starttime}\n stoptime = {self.stoptime}\n \
+stdout = {self.stdout}\n stderr = {self.stderr}\n env = {self.env}\n failure = {self.failure}\n \
+fatal = {self.fatal}\n logs = {self.logs}\n"
+
