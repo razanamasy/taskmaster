309ec8f816dc73d860df2a388f5631547d72c935
alice <alice@Kfs.com>
2023-05-01 07:02:47 -0700

    modifyed parsing

--- server/parse.py
+++ server/parse.py
@@ -2,11 +2,7 @@ import yaml
 import sys
 from process_struct import *
 
-<<<<<<< HEAD
-def parse_file(configs):
-=======
 def parse_file(configs, client_socket):
->>>>>>> master
     process_dict = {}
     if "programs" in configs == False:
         print("No programs section")
@@ -16,21 +12,13 @@ def parse_file(configs, client_socket):
             if isinstance(programs, dict) and programs != "programs":
                 for proc_name, conf in programs.items():
                     if isinstance(conf, dict):
-<<<<<<< HEAD
-                        process_dict[proc_name] = process(proc_name)
-=======
-                        print("Proc name in parsing : ", proc_name)
                         process_dict[proc_name] = process_data(proc_name)
->>>>>>> master
+                        setattr(process_dict[proc_name], "client", client_socket)
                         for key, value in conf.items():
                             if isinstance(value, dict):
                                 print("Wrong config file3")
                                 exit(1)
                             else:
-<<<<<<< HEAD
-=======
-                                print("key and value : ", "|",key,"| " "|",value,"| ")
->>>>>>> master
                                 setattr(process_dict[proc_name], key, value)
                     else:
                         print("Wrong config file2")
@@ -38,17 +26,6 @@ def parse_file(configs, client_socket):
             else:
                 print("Wrong config file1")
                 exit(1)
-<<<<<<< HEAD
-    print(process_dict["ls"]);
-    print(process_dict["env"]);
-        
-
-def open_file(conf_file):
-    try:
-        with open(conf_file, 'r') as f:
-            configs = yaml.safe_load(f)
-            parse_file(configs)
-=======
     client_proc_dict = {client_socket: process_dict}
     print(client_proc_dict[client_socket]["while"])
     return (client_proc_dict)
@@ -60,7 +37,6 @@ def open_file(conf_file, client_socket):
         with open(conf_file, 'r') as f:
             configs = yaml.safe_load(f)
             return parse_file(configs, client_socket)
->>>>>>> master
     except FileNotFoundError:
         print("File not found")
         exit(1)
@@ -71,13 +47,6 @@ def open_file(conf_file, client_socket):
         print("You don't have the permissions to read the file")
         exit(1)
 
-<<<<<<< HEAD
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Wrong number of arguments")
-        exit(1)
-    open_file(sys.argv[1]);
-=======
 def main(arg1, arg2):
     return open_file(arg1, arg2);
 
@@ -86,5 +55,3 @@ if __name__ == "__main__":
         print("Wrong number of arguments")
         exit(1)
     main(sys.argv[1], sys.argv[2])
-
->>>>>>> master
---
+++ server/server.py
@@ -0,0 +1,93 @@
+import socket
+import sys
+import os
+import select
+from create_child_process import main as main_launch
+from parse import main as main_parse
+
+# Define the host and port to listen on
+HOST = 'localhost'
+PORT = 12345
+
+# Create a socket object and bind it to the host and port
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+
+# Listen for incoming connections
+server_socket.listen()
+
+# Create a list to keep track of clients
+clients = []
+
+print(f"Server listening on {HOST}:{PORT}")
+
+# Set up the poll object
+poll_object = select.poll()
+poll_object.register(server_socket, select.POLLIN)
+
+
+while True:
+    # Wait for events from clients or the server socket
+    events = poll_object.poll()
+
+    for fd, event in events:
+        # If the event is from the server socket, it means there's a new connection
+        if fd == server_socket.fileno():
+            client_socket, addr = server_socket.accept()
+            clients.append(client_socket)
+            poll_object.register(client_socket, select.POLLIN)
+            print(f"New client connected from {addr[0]}:{addr[1]}")
+            path_conf = client_socket.recv(1024).decode()
+            print("LAUNH SERVER HERE received WARNING IT LAUNCHES 5 WHILE: ", path_conf)
+			#the first parsing for launch here
+			
+            list_proc_data = main_parse(path_conf, client_socket) #ici retourner un element proc_data = process_data
+
+			#Boucler sur le tableau de structure de process a envoyer au launch
+			#Boucler et le stocker quelque part
+            for key in list_proc_data:
+                main_launch(list_proc_data[key])
+            #main_launch(list_proc_data["while"]) #main_launch(process_structure)
+
+        # If the event is from a client socket, it means there's data to read
+        elif event & select.POLLIN:
+            client_socket = None
+            for client in clients:
+                if client.fileno() == fd:
+                    client_socket = client
+                    break
+            if client_socket is None:
+                continue
+
+            data = client_socket.recv(1024).decode()
+
+            # Process the job command
+            if data == 'start':
+                print("Starting the job...")
+                # Code to start the job goes here
+                result = "starting the job..." 
+            elif data == 'stop':
+                print("Stopping the job...")
+                # Code to stop the job goes here
+                result = "Stopping the job..."
+            elif data == 'quit':
+                print("Shutdown")
+                result = "server shutdown"
+                client_socket.sendall(result.encode())
+                poll_object.unregister(client_socket)
+                clients.remove(client_socket)
+                client_socket.close()
+            else:
+                print("Invalid command.", data)
+                result = "Invalid command."
+
+            # Send the result back to the client
+            client_socket.sendall(result.encode())
+
+# Close the client connections and the server socket
+for client in clients:
+    poll_object.unregister(client)
+    client.close()
+server_socket.close()
+conf_file.close()
