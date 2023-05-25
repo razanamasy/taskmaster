02e39b91edc3cef5e8a4324ac731880ded413e94
razanamasy <hina.razanamasy@gmail.com>
2023-05-25 10:46:37 +0200

    test ls starttime 0 + reload return

--- server/create_child_process.py
+++ server/create_child_process.py
@@ -10,10 +10,10 @@ def main(data):
 #    print("In create_child file proc data name is : ", data.name)
 #    print("In create_child the cmd to execute is : ", data.cmd)
     pids = []
-    stdout_original = sys.stdout
-    stderr_original = sys.stderr
-    stdout_fd = os.dup(stdout_original.fileno())
-    stderr_fd = os.dup(stderr_original.fileno())
+ #   stdout_original = sys.stdout
+ #   stderr_original = sys.stderr
+ #   stdout_fd = os.dup(stdout_original.fileno())
+ #   stderr_fd = os.dup(stderr_original.fileno())
     for i in range(1):
         pid = os.fork()
         if pid == 0:
@@ -35,26 +35,26 @@ def main(data):
                 
                 os.environ = child_env
                 
-                stdout_file = open(data.stdout, "a")
-                stderr_file = open(data.stderr, "a")
-                os.dup2(stdout_file.fileno(), stdout_original.fileno())
-                os.dup2(stderr_file.fileno(), stderr_original.fileno())
+           #     stdout_file = open(data.stdout, "a")
+           #     stderr_file = open(data.stderr, "a")
+           #     os.dup2(stdout_file.fileno(), stdout_original.fileno())
+           #     os.dup2(stderr_file.fileno(), stderr_original.fileno())
 
                 command = data.cmd.split()
                 os.execve(command[0], command, os.environ)
             except Exception as e:
-                os.dup2(stdout_file.fileno(), stdout_original.fileno())
-                os.dup2(stderr_file.fileno(), stderr_original.fileno())
-                print(str(e), file=stderr_file)
-                if stdout_fd is not None:
-                    os.dup2(stdout_fd, stdout_original.fileno())
-                    os.close(stdout_fd)
-                if stderr_fd is not None:
-                    os.dup2(stderr_fd, stderr_original.fileno())
-                    os.close(stderr_fd)
-                sys.stdout = stdout_original
-                sys.stderr = stderr_original
-                print(str(e), file=sys.stdout)
+          #      os.dup2(stdout_file.fileno(), stdout_original.fileno())
+          #      os.dup2(stderr_file.fileno(), stderr_original.fileno())
+          #      print(str(e), file=stderr_file)
+          #      if stdout_fd is not None:
+          #          os.dup2(stdout_fd, stdout_original.fileno())
+          #          os.close(stdout_fd)
+          ##      if stderr_fd is not None:
+          #          os.dup2(stderr_fd, stderr_original.fileno())
+          #          os.close(stderr_fd)
+          #      sys.stdout = stdout_original
+          #      sys.stderr = stderr_original
+          #      print(str(e), file=sys.stdout)
                 
                 sys.exit(1)  # Terminate child process with error status
                 # This line will never be reached
